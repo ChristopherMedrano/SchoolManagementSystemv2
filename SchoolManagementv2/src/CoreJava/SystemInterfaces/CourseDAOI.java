8c7ee683dde992392d843136ab80729a85c45034
Christopher Medrano <cmedrano625@gmail.com>
2018-11-14 16:54:19 -0500

    updated interface

--- SchoolManagementv2/src/CoreJava/SystemInterfaces/CourseDAOI.java
+++ SchoolManagementv2/src/CoreJava/SystemInterfaces/CourseDAOI.java
@@ -1,5 +1,11 @@
 package CoreJava.SystemInterfaces;
 
+import java.util.List;
+
+import CoreJava.Models.Course;
+
 public interface CourseDAOI {
 	
+	List<Course> getAllCourses();
+	List<Course> getCourseByInstructor();
 }
